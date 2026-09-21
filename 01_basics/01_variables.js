1832023472ca503209e2200cd8464c1ed5c5aeb6
dimplesree11 <dimplesree11@gmail.com>
2026-09-21 11:17:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let  accountEmail = "dimple@google.com"
+var accountPassword = "12345"
+accountCity = "Tirupati"
+let accountState;
+// accountId = 2
+accountEmail = "ds@google.com"
+accountPassword = "2312"
+accountCity = "Bangalore" 
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
